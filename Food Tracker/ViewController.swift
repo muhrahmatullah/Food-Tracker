32a593b65365ab69c0403736d78ba37ab3d94a34
Muh Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-09-06 00:07:44 +0700

    add actions and outlets

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Mark: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //Mark: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 }
 
